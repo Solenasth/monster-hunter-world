fd0d5877c9a5b319a5b51384db02976bab46819c
kobo <solenasthellion@gmail.com>
2019-05-27 21:32:05 -0500

    fixed package.json, added stat elements wip

---
+++ src/Stat_elements.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { common_icons } from "./ui_common_icons";
+
+class Stat_elements extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { resistances } = this.props;
+    return (
+    
+      {//wip}
+    );
+  }
+}
+
+export default Stat_elements;
