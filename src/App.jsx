c66ca117408bb0a242445447dda50fd7a39d3961
carlos alcala <solenasthellion@gmail.com>
2019-06-11 15:51:26 -0500

    work in progress resultscards

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import { render } from "react-dom";
 import SearchBox from "./SearchBox.jsx";
 import Results from "./Results.jsx";
 import Axios from "axios";
-
-//import { Router, Link } from "@reach/router";
+import { Router, Link } from "@reach/router";
 
 class App extends React.Component {
   constructor(props) {
@@ -21,6 +20,27 @@ class App extends React.Component {
     );
   };
 
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  //gets an initial entry to show
+  getInitial = () => {
+    Axios.get(
+      this.url +
+        this.state.primarySearch +
+        '?q={"id":' +
+        this.getRandomInt(0, 1200) +
+        "}"
+    ).then(details => this.setState({ details: details.data }));
+  };
+
+  getDetails = id => {
+    Axios.get(
+      this.url + this.state.primarySearch + '?q={"id":' + id + "}"
+    ).then(details => this.setState({ details: details.data }));
+  };
+
   handlePrimarySearchChange = event => {
     this.setState({ primarySearch: event.target.value });
   };
@@ -28,11 +48,29 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <SearchBox
-          search={this.getOneLameSword}
-          searchChange={this.handlePrimarySearchChange}
-        />
-        <Results type={this.state.primarySearch} results={this.state.results} />
+        <header className="header">
+          <Link to="/" className="header__title">
+            <span>MHW</span>
+          </Link>
+          <Link to="/search" className="header__search">
+            <span aria-label="search" role="img">
+              🔍
+            </span>
+          </Link>
+        </header>
+        <Router>
+          <SearchBox
+            path="/search"
+            search={this.getOneLameSword}
+            searchChange={this.handlePrimarySearchChange}
+          />
+          <Results
+            path="/"
+            type={this.state.primarySearch}
+            results={this.state.results}
+            getDetails={this.getInitial}
+          />
+        </Router>
       </div>
     );
   }
--- src/Results.jsx
+++ src/Results.jsx
@@ -7,27 +7,36 @@ class Results extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.getDetails(1);
+  }
+
   handleResults(results, type) {
-    
     if (results && Array.isArray(results)) {
       switch (type) {
         case "weapons": {
-          return results.map(weapon => <WeaponDetails weapon={weapon} key={weapon.id}/>)
+          return results.map(weapon => (
+            <WeaponDetails weapon={weapon} key={weapon.id} />
+          ));
         }
         case "armor":
           return results.map(armor => {
             return <ArmorDetails armor={armor} key={armor.id} />;
           });
         default:
-          return <h1>nothing to display 1</h1>;
+          return <h1>Oh no, the api returned something i cant handle yet</h1>;
       }
     } else {
-      return <h1>nothing to display 2</h1>;
+      return this.props.details ? (
+        <WeaponDetails weapon={this.state.details[0]} />
+      ) : (
+        <h1>loading</h1>
+      );
     }
   }
 
   render() {
-    const { type, results } = this.props;
+    const { type, results, getDetails } = this.props;
     return <div>{this.handleResults(results, type)}</div>;
   }
 }
